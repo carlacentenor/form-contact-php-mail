dd1e7712ee8a5a227b1befb7a3f17729c300461e
carlacentenor <carlacentenor@gmail.com>
2020-04-11 21:53:52 -0500

    add changes

--- sendemailphp/enviarprueba.php
+++ sendemailphp/enviarprueba.php
@@ -23,14 +23,14 @@ try {
     $mail->isSMTP();                                            // Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'carlicentenor@gmail.com';                     // SMTP username
-    $mail->Password   = 'carlita71288';                               // SMTP password
+    $mail->Username   = 'xxxxxxxx@gmail.com';                     // SMTP username
+    $mail->Password   = 'xxxxxxxxx';                               // SMTP password
     $mail->SMTPSecure = 'tls';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
     //Recipients
-    $mail->setFrom('carlicentenor@gmail.com', 'Web'); 
-    $mail->addAddress('carlicentenor@gmail.com', 'Contacto');//correo desde el cual se enviara el mail
+    $mail->setFrom('xxxxxxxx@gmail.com', 'Web'); 
+    $mail->addAddress('xxxxxxxx@gmail.com', 'Contacto');//correo desde el cual se enviara el mail
    
 
     // Content
